7b1ed4d6c4a2be14ece1484901512d59262c460c
hmerritt <harry@mrrtt.me>
2024-12-17 02:02:03 +0000

    chore: update example env

--- examples/react-webpack/src/App.js
+++ examples/react-webpack/src/App.js
@@ -2,7 +2,7 @@ const App = () => (
 	<div>
 		<h1>reactenv</h1>
 		<p>
-			REACT_APP_SECRET: <strong>{process.env.REACT_APP_SECRET}</strong>
+			REACT_APP_VAR: <strong>{process.env.REACT_APP_VAR}</strong>
 		</p>
 		<p>
 			REACT_APP_API_URL: <strong>{process.env.REACT_APP_API_URL}</strong>
